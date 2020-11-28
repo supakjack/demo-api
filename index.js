3b2699226c138d45e4574bd5bae4021573529964
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-11-29 00:36:22 +0700

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const app = express()
-const port = 80
+const port = process.env.PORT || 80
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  const message = "Hello World!"
+  res.send({ message })
 })
 
 app.get("/api", (req, res) => {
-  res.send("Hello World API!")
+  const message = "Hello World API!!"
+  res.send({ message })
 })
 
 app.listen(port, () => {
